75cd0e856ab0769c5c760ffa8bf4ba62643eae49
lacey <hiu@example.com>
2019-02-15 12:01:16 +0000

    create and test vendor class

--- src/main/java/Vendor.java
+++ src/main/java/Vendor.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Vendor {
 
-    private HashMap<String, Integer> stockList;
+    private ArrayList<Stock> stockList;
 
     public Vendor(){
-        this.stockList = new HashMap<>();
+        this.stockList = new ArrayList<>();
     }
+
+    public int getStockListCount(){
+        return this.stockList.size();
+    }
+
+    public void addToStockList(Stock stock){
+        this.stockList.add(stock);
+    }
+
+    public void removeStock(){
+        this.stockList.remove(0);
+    }
+
+
 }
--- src/test/java/VendorTest.java
+++ src/test/java/VendorTest.java
@@ -5,14 +5,31 @@ import static org.junit.Assert.assertEquals;
 
 public class VendorTest {
     Vendor vendor;
+    Stock pretzels;
+
 
     @Before
     public void before(){
         vendor = new Vendor();
+        pretzels = new Stock("pretzels", 0.50);
     }
 
     @Test
     public void canCheckStock(){
-        assertEquals("pretzels", )
+        assertEquals("pretzels", vendor.getStockListCount());
+    }
+
+    @Test
+    public void canAddToStock(){
+        vendor.addToStockList(pretzels);
+        assertEquals(1, vendor.getStockListCount());
+    }
+
+    @Test
+    public void canRemoveFromStock(){
+        vendor.addToStockList(pretzels);
+        vendor.removeStock();
+        assertEquals(0, vendor.getStockListCount());
+
     }
 }
